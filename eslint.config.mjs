119a20ccc8074bee662fa79477e505b805ec5164
Credo Revolution <sadistik871@mail.ru>
2026-01-14 02:05:06 +0300

    Set up project with Nuxt, TailwindCSS, DaisyUI, Prettier, and ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+
+export default withNuxt()
+  .append({
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  })
+  .append(eslintConfigPrettier)
