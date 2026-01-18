33ddfee76fa1f8d01ca102d79c75c46f663941b4
Alex_Gitignore <sadistik871m@gmail.com>
2026-01-18 19:26:41 +0300

    Add initial UI components and layout for Travel Log app

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="navbar-start">
+    <div class="navbar bg-primary text-primary-content">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">Travel Log</NuxtLink>
+    </div>
+  </div>
+  <div class="navbar-end">
+    <button class="btn btn-accent">Sign In With GitHub<Icon name="tabler:brand-github" size="24"/></button>
+  </div>
+</template>
+
+<style scoped></style>
